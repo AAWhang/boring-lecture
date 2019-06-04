e4ebdbb5c38aaf1f84ff028483f0d284e1b96b8f
Aaron Whang and Jessica Munoz <jessica@>
2019-06-04 13:24:33 -0700

    added clickable paragraph that will highlight it and add border

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-  $("p").click(function(){
-    $("p").removeClass();
-    $("p").addClass("paragraphStyle");
+
+
+//click once on P to show css element and click again to hide it
+  $(".firstParagraph").click(function(){
+      $("p#first").removeClass();
+      $("p#first").addClass("paragraphStyle");
+
+      $(".firstParagraph").click(function(){
+      $("p#first").removeClass();
+    });
+  });
+
+
+//click on p to show css element
+  $(".secondParagraph").click(function(){
+    $("p#second").removeClass();
+    $("p#second").addClass("paragraphStyle");
   });
 
 
